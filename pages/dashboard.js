46be3ccc19d553c57d6575612d01977542498292
Manish Shivabhakti <manish@learnr.app>
2022-10-04 22:17:55 +0545

    Init dashboard route

---
+++ pages/dashboard.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+      <h1 className='font-semibold'>Dashboard</h1>
+    </div>
+  );
+}
