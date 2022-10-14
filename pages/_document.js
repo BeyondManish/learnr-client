643a3a737d812c49a08141c39b88b7d6aa281fbc
Manish Shivabhakti <manish@learnr.app>
2022-10-14 09:17:44 +0545

    Update font

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, NextScript, Main } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
-      <Head />
-      <body>
+    <Html className='h-full'>
+      <Head>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+      </Head>
+      <body className='h-full bg-gray-200'>
         <Main />
         <NextScript />
       </body>
