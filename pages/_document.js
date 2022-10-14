3ab856e104693090ba1a4290e3875295f8d83f85
Manish Shivabhakti <manish@learnr.app>
2022-10-14 21:36:02 +0545

    Update cdn

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='en' className='h-full'>
       <Head>
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/inter-ui/3.19.3/inter.min.css"></link>
       </Head>
       <body className='h-full bg-gray-200'>
         <Main />
