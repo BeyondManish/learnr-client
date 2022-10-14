5e55ce3d5b971260d6a1b251525168badbcd4975
Manish Shivabhakti <manish@learnr.app>
2022-10-14 11:03:24 +0545

    Add success banner

--- components/Banner.js
+++ components/Banner.js
@@ -5,3 +5,11 @@ export function ErrorBanner({ message }) {
     </div>
   );
 }
+
+export function SuccessBanner({ message }) {
+  return (
+    < div className="flex justify-between w-full p-2 mb-4 text-sm font-semibold text-white bg-green-600 rounded-md ring-1 ring-green-600">
+      <p>{message}</p>
+    </div>
+  );
+}
