6febaa9cb59d5e54c2b5f41eb4b61a34e8a50b3c
Manish Shivabhakti <manish@learnr.app>
2022-10-14 09:17:08 +0545

    Add forgot-password page

---
+++ pages/forgot-password.js
@@ -0,0 +1,11 @@
+import ForgotPasswordForm from "../components/forms/ForgotPassword";
+import MainLayout from "../components/layout/MainLayout";
+
+export default function RegisterPage() {
+  return (
+    // Login form
+    <MainLayout>
+      <ForgotPasswordForm />
+    </MainLayout>
+  );
+}
