c23774eb19c26bfaebafc8c283b3d13340b53d84
Manish Shivabhakti <manish@learnr.app>
2022-10-14 09:18:39 +0545

    Add admin dashboard

---
+++ pages/admin/dashboard.js
@@ -0,0 +1,13 @@
+import AdminLayout from "../../components/layout/AdminLayout";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <AdminLayout>
+        <div>
+          Admin Page
+        </div>
+      </AdminLayout>
+    </>
+  );
+}
