3a57acc7820b6a91dc13613cbfb269a4fa9eec98
Manish Shivabhakti <manish@learnr.app>
2022-10-14 09:18:19 +0545

    Add register page

---
+++ pages/register.js
@@ -0,0 +1,11 @@
+import RegisterForm from "../components/forms/RegisterForm";
+import MainLayout from "../components/layout/MainLayout";
+
+export default function RegisterPage() {
+  return (
+    // Login form
+    <MainLayout>
+      <RegisterForm />
+    </MainLayout>
+  );
+}
