e254f4ce23eb984f66b6f7fab65bfe61382450b5
Manish Shivabhakti <manish@learnr.app>
2022-10-16 23:02:05 +0545

    Fix typo

--- components/Badges.js
+++ components/Badges.js
@@ -7,7 +7,7 @@ export default function Badge({ name, onClick }) {
         onClick={onClick}
         className="flex-shrink-0 ml-0.5 h-4 w-4 rounded-full inline-flex items-center justify-center text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:outline-none focus:bg-indigo-500 focus:text-white"
       >
-        <span className="sr-only">Remove large option</span>
+        <span className="sr-only">Remove option</span>
         <svg className="w-2 h-2" stroke="currentColor" fill="none" viewBox="0 0 8 8">
           <path strokeLinecap="round" strokeWidth="1.5" d="M1 1l6 6m0-6L1 7" />
         </svg>
