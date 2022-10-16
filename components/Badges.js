8b3721ecdf635067fa129e876e5d09c0abd9f4e2
Manish Shivabhakti <manish@learnr.app>
2022-10-16 22:43:45 +0545

    Fix only close on x-icon click

--- components/Badges.js
+++ components/Badges.js
@@ -1,9 +1,10 @@
 export default function Badge({ name, onClick }) {
   return (
-    <span onClick={onClick} className="inline-flex rounded-full items-center py-0.5 pl-2.5 pr-1 text-sm font-medium bg-indigo-100 text-indigo-700 mr-2">
+    <span className="inline-flex rounded-full items-center py-0.5 pl-2.5 pr-1 text-sm font-medium bg-indigo-100 text-indigo-700 mr-2">
       {name}
       <button
         type="button"
+        onClick={onClick}
         className="flex-shrink-0 ml-0.5 h-4 w-4 rounded-full inline-flex items-center justify-center text-indigo-400 hover:bg-indigo-200 hover:text-indigo-500 focus:outline-none focus:bg-indigo-500 focus:text-white"
       >
         <span className="sr-only">Remove large option</span>
