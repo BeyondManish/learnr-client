ba536fecbe57d4e7a4edba657680d25bee90bb48
Manish Shivabhakti <manish@learnr.app>
2022-10-17 21:54:44 +0545

    Fix auth from localStorage

--- context/Auth.js
+++ context/Auth.js
@@ -1,18 +1,26 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [auth, setAuth] = useState({
-    user: undefined,
-    token: undefined
+    user: null,
+    token: ""
   });
 
   useEffect(() => {
     if (localStorage.getItem('auth')) {
-      JSON.parse(localStorage.getItem('auth'));
+      setAuth(JSON.parse(localStorage.getItem('auth')));
     }
   }, []);
+
+  // configure axios defaults
+  if (process.server) {
+    axios.defaults.baseURL = process.env.API; // the client is running in server
+  } else {
+    axios.defaults.baseURL = process.env.NEXT_PUBLIC_API;
+  }
 
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
