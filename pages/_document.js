ee522664c564d4998a55f775c58a82c93326ea97
Manish Shivabhakti <manish@learnr.app>
2022-10-20 21:59:51 +0545

    Add dark class on body

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/inter-ui/3.19.3/inter.min.css"></link>
       </Head>
-      <body className='h-full bg-gray-200'>
+      <body className='h-full bg-gray-200 dark:bg-gray-800 dark:text-gray-50'>
         <Main />
         <NextScript />
       </body>
