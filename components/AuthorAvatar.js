5319e3e30943fa17ca6e5becde23c052a6e07a99
Manish Shivabhakti <manish@learnr.app>
2022-10-20 21:55:12 +0545

    Add author avatar component

---
+++ components/AuthorAvatar.js
@@ -0,0 +1,11 @@
+export default function AuthorAvatar({ image }) {
+  return (
+    <div className="w-10 h-10">
+      <img
+        className="inline-block w-10 h-10 rounded-full"
+        src={image}
+        alt=""
+      />
+    </div>
+  );
+}
