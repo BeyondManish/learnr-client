5f0a81c6130d1043f30bd03e3bc8e2975da9d81f
Manish Shivabhakti <manish@learnr.app>
2022-10-20 21:58:53 +0545

    Add authorization, error or success message banner

--- pages/admin/categories/index.js
+++ pages/admin/categories/index.js
@@ -1,25 +1,25 @@
 import { useState } from "react";
 import AdminLayout from "../../../components/layout/AdminLayout";
-import { ErrorBanner } from "../../../components/Banner";
+import { ErrorBanner, SuccessBanner } from "../../../components/Banner";
 import axios from "axios";
 
 export default function CategoriesAdminPage() {
   const [state, setState] = useState({
     name: "",
     error: "",
+    success: ""
   });
 
-  const { name, error } = state;
+  const { name, error, success } = state;
 
   const updateValues = (e) => {
-    setState({ ...state, name: e.target.value, error: "" });
+    setState({ ...state, name: e.target.value, error: "", success: "" });
   };
 
   const submitFrom = (e) => {
     e.preventDefault();
-    console.table({ name });
-    axios.post(`http://localhost:8000/api/v1/category/create`, { name }).then((response) => console.log(response.data))
-      .catch((err) => { console.log(err.response.data); setState({ ...state, error: err.response.data.message }); });
+    axios.post(`category/create`, { name }).then((response) => setState({ ...state, success: response.data.message, error: "" }))
+      .catch((err) => { console.log(err.response.data); setState({ ...state, error: err.response.data.message, success: "" }); });
   };
 
   return (
@@ -29,7 +29,12 @@ export default function CategoriesAdminPage() {
       <div className="p-6 bg-white rounded-md">
         <form>
           <div className="w-full lg:max-w-[70%]">
-            {error && <ErrorBanner message={error} />}
+            {error && (
+              <ErrorBanner message={state.error} />
+            )}
+            {success && (
+              <SuccessBanner message={state.success} />
+            )}
           </div>
           <div>
             <label htmlFor="category" className="block mb-2 font-medium text-gray-700">
@@ -47,7 +52,7 @@ export default function CategoriesAdminPage() {
           </div>
           <div className="mt-4">
             <button
-              type="button"
+              type="submit"
               onClick={submitFrom}
               className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
