17268ec079f984e3e7beaf6b44eaada3519827ea
Manish Shivabhakti <manish@learnr.app>
2022-10-29 21:42:24 +0545

    Add padding to the main

--- components/layout/MainLayout.js
+++ components/layout/MainLayout.js
@@ -4,7 +4,7 @@ export default function MainLayout({ children }) {
   return (
     <>
       <MainNav />
-      <main className="h-full">
+      <main className="h-full pt-24 md:px-4">
         {children}
       </main>
     </>
