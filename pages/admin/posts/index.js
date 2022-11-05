ef8a615a08b9de2bd4039b5379472b5027f826df
Manish Shivabhakti <manish@learnr.app>
2022-11-05 20:46:38 +0545

    Show basic post

--- pages/admin/posts/index.js
+++ pages/admin/posts/index.js
@@ -1,7 +1,24 @@
 import Head from "next/head";
 import AdminLayout from "../../../components/layout/AdminLayout";
+import axios from 'axios';
+import { useState, useEffect } from "react";
+import { Button } from "../../../components/Buttons";
+import { useRouter } from 'next/router';
 
 export default function PostPage() {
+
+  const router = useRouter();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('/posts')
+      .then(res => {
+        console.log(res.data.data.posts);
+        setPosts(res.data.data.posts);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <>
       <Head>
@@ -10,9 +27,20 @@ export default function PostPage() {
         </title>
       </Head>
       <AdminLayout>
-        <div>
-          This is posts page
-        </div>
+        {posts.length > 0 ?
+          posts.map(post => (
+            <div key={post._id}>
+              <h1>{post.title}</h1>
+              <p>{post.author.firstname}</p>
+              <p>{post.content}</p>
+              <br />
+            </div>
+          )) :
+          (<div>
+            <h1 className="mb-4 text-lg font-medium">No posts yet</h1>
+            <Button text="Create post" onClick={() => router.push("/admin/posts/create")} />
+          </div>
+          )}
       </AdminLayout>
     </>
   );
