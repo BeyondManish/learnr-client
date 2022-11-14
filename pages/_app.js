e3582373cdfe9d2b8e5aa9276cea936003aff8ba
Manish Shivabhakti <manish@learnr.app>
2022-11-14 18:20:27 +0545

    Add post and media

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import { AuthProvider } from "../context/Auth";
+import PostDataProvider from "../context/Post";
 import { ThemeProvider } from "../context/Theme";
 import "../styles/globals.css";
+import { MediaProvider } from "../context/Media";
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <Component {...pageProps} />
+        <PostDataProvider>
+          <MediaProvider>
+            <Component {...pageProps} />
+          </MediaProvider>
+        </PostDataProvider>
       </AuthProvider>
     </ThemeProvider>
   );
