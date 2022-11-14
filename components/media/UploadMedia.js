15e1b5c4266613a86f7f32ed0cb20e15fcd6e01d
Manish Shivabhakti <manish@learnr.app>
2022-11-14 18:11:19 +0545

    Add upload feature

--- components/media/UploadMedia.js
+++ components/media/UploadMedia.js
@@ -1,11 +1,38 @@
+import { useContext, useState } from "react";
+import { uploadImage } from "../../functions/upload";
+import { MediaContext } from "../../context/Media";
+import Image from "next/image";
+
 export default function Media() {
+  // context
+  const [media, setMedia] = useContext(MediaContext);
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="flex items-center justify-center w-full">
       <input
         type="file"
+        onChange={(e) => {
+          setLoading(!loading);
+          uploadImage(e.target.files[0]).then((res) => {
+            console.log(res);
+            setMedia({
+              images: [res.url],
+              selected: res.url,
+              showMediaModal: false,
+            });
+            localStorage.setItem("postFeaturedImage", JSON.stringify(res));
+            setLoading(!loading);
+          });
+        }}
         id="uploadFile" hidden
       />
-      <label className="flex items-center justify-center w-full h-40 px-4 py-2 text-base font-medium border-4 border-dashed rounded-md focus:outline-none"
-        for="uploadFile">Upload image file</label>
+      {
+        loading ? (<div>
+          Uploading...
+        </div>) :
+          <label className="flex items-center justify-center w-full h-40 px-4 py-2 text-base font-medium border-4 border-dashed rounded-md focus:outline-none"
+            htmlFor="uploadFile">Upload image file</label>
+      }
     </div>);
 };;
