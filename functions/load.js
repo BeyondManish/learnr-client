5b0135beaea8c319c4e51c489cb86eb8cd0b54c2
Manish Shivabhakti <manish@learnr.app>
2022-11-14 18:19:15 +0545

    Add loadPosts, loadPost, loadAllMedias

--- functions/load.js
+++ functions/load.js
@@ -4,3 +4,18 @@ export const loadCategories = async () => {
   const response = await axios.get('/categories');
   return response.data;
 };
+
+export const loadPosts = async () => {
+  const response = await axios.get('/posts');
+  return response.data;
+};
+
+export const loadPost = async (author, slug) => {
+  const response = await axios.get(`/${author}/${slug}`);
+  return response.data;
+};
+
+export const loadAllMedias = async () => {
+  const response = await axios.get('/files');
+  return response.data;
+};
