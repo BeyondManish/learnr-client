cfc4aafd33335b606cb29103dafc2ac326790856
Manish Shivabhakti <manish@learnr.app>
2022-11-16 21:10:44 +0545

    Add server-side render

--- pages/[author]/[slug].js
+++ pages/[author]/[slug].js
@@ -1,24 +1,22 @@
 import Content from "../../components/posts/Content";
 import MainLayout from "../../components/layout/MainLayout";
 import CommentForm from "../../components/forms/CommentForm";
-import { useRouter } from "next/router";
 import axios from "axios";
+import Head from 'next/head';
 
-export default function BlogPost() {
-
-  const router = useRouter();
-  const { author, slug } = router.query;
-  console.log(author, slug);
-  let post;
-  axios.get(`/${author}/${slug}`).then(({ data }) => post = data.data.post).catch((err) => { console.log(err); });
-  console.log(post);
+export default function BlogPost({ post }) {
   return (
     <MainLayout>
+      <Head>
+        <title>
+          {post.title} | Learnr
+        </title>
+      </Head>
       <div className="flex flex-col max-w-3xl mx-auto">
         <div className="">
-          {/* <Content post={post} /> */}
+          <Content post={post} />
         </div>
-
+        <h2 className="my-4 text-xl font-medium">Comments</h2>
         <div className="px-8 mb-4 bg-white border-gray-300 rounded-md dark:bg-gray-900 dark:border-gray-700">
           <h2 className="my-4 text-xl font-medium">Leave a comment</h2>
           <CommentForm postId="asb" />
@@ -26,4 +24,13 @@ export default function BlogPost() {
       </div>
     </MainLayout>
   );
+}
+
+export async function getServerSideProps({ params }) {
+  const { data } = await axios.get(`/post/${params.slug}`);
+  return {
+    props: {
+      post: data.data.post,
+    }
+  };
 }
