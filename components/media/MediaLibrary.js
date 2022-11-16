a6c8bc512307cc1d89a7affce40f9cba24dbbf2f
Manish Shivabhakti <manish@learnr.app>
2022-11-16 22:26:29 +0545

    Add media library

--- components/media/MediaLibrary.js
+++ components/media/MediaLibrary.js
@@ -1,5 +1,29 @@
+import { useContext, useEffect } from 'react';
+import { MediaContext } from "../../context/Media";
+import { loadAllMedias } from '../../functions/load';
+import { Button } from '../Buttons';
+import { TrashIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
+
 export default function MediaLibrary() {
-  return (<div>
-    <h1>Media Library</h1>
-  </div>);
+  const [media, setMedia] = useContext(MediaContext);
+
+  useEffect(() => {
+    loadAllMedias().then((res) => {
+      setMedia({ ...media, images: res.medias });
+    });
+  }, []);
+
+
+  console.log(media);
+  return (
+    <div className='flex flex-wrap'>
+      {(media.images.length > 0) ?
+        (media.images.map((image) => (
+          <div key={image._id} onClick={() => setMedia((prev) => ({ ...prev, selected: image, showMediaModal: false }))} className='relative h-40 max-w-xs m-2 overflow-hidden border border-gray-300 rounded-lg cursor-pointer dark:border-gray-700'>
+            {/* <Button className={`absolute z-10 top-1 right-1`} icon={<TrashIcon />} onClick={() => deleteMedia(image._id)} /> */}
+            <Image className="object-cover w-full" width={720} height={400} src={image.url} alt={image.name} />
+          </div>
+        ))) : (<p>No media found</p>)}
+    </div>);
 }
