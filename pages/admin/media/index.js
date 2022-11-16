5dfa48eb11609e5624d8774705054f78d43b91a0
Manish Shivabhakti <manish@learnr.app>
2022-11-16 21:11:40 +0545

    Add media context

--- pages/admin/media/index.js
+++ pages/admin/media/index.js
@@ -1,36 +1,54 @@
 import AdminLayout from '../../../components/layout/AdminLayout';
 import Head from 'next/head';
-import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { useState, useEffect, useContext } from 'react';
 import { loadAllMedias } from '../../../functions/load';
+import { Button } from '../../../components/Buttons';
+import { TrashIcon } from '@heroicons/react/24/outline';
+import { MediaContext } from '../../../context/Media';
+import axios from 'axios';
 
 export default function MediaPage() {
 
-  const [medias, setMedias] = useState([]);
+  const [media, setMedia] = useContext(MediaContext);
   const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
 
   useEffect(() => {
     loadAllMedias().then((res) => {
-      console.log(res.medias);
-      setMedias(res.medias);
+      setMedia(prev => ({ ...prev, images: res.medias }));
+    }).catch(err => {
+      setError(err.response.data?.message || err.response.data?.errors[0] || "Error fetching medias");
     });
   }, []);
+
+  console.log(media);
+  // TODO: add a delete button
+  const deleteMedia = (id) => {
+    axios.delete(`/files/${id}`).then(res => {
+      console.log(res);
+      setMedia(prev => ({ ...prev, images: prev.images.filter(image => image._id !== id) }));
+    });
+    console.log(media);
+  };
+
+  console.log(media);
 
   return (
     <AdminLayout>
       <Head>
         <title>Media | Learnr</title>
       </Head>
-      <h1 className='admin-topic-heading'>All Medias</h1>
-      <div>
+      <h1 className='mb-2 admin-topic-heading'>All Medias</h1>
+      <div className='flex flex-wrap'>
         {
-          medias.map((media) => (
-            <div key={media._id}>
-              <img src={media.url} alt={media.name} />
+          media.images.map((image) => (
+            <div key={image._id} className='relative max-w-sm m-2 overflow-hidden border border-gray-300 rounded-lg h-52 dark:border-gray-700'>
+              <Button className={`absolute z-10 top-1 right-1`} icon={<TrashIcon />} onClick={() => deleteMedia(image._id)} />
+              <Image className="object-cover w-full" width={720} height={400} src={image.url} alt={image.name} />
             </div>
           ))
         }
       </div>
-    </AdminLayout>
+    </AdminLayout >
   );
 }
