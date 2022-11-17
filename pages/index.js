f278c71c7d2437b7c3d3c64bcdcd3899bb7d536c
Manish Shivabhakti <manish@learnr.app>
2022-11-17 07:41:38 +0545

    Edit homepage for no post and categories

--- pages/index.js
+++ pages/index.js
@@ -26,38 +26,43 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainLayout>
-        <div className='flex justify-center w-full h-full text-gray-900 md:px-4 dark:text-gray-100'>
-          <div className='w-full md:w-3/5 lg:w-2/5'>
-            {
-              postData.posts.map((post) => (
-                <Card key={post.slug} post={post} />
-              ))
-            }
-          </div>
-          <aside className='hidden md:block'>
-            <div className='ml-6'>
-              {postData.categories.length > 0 ?
-                (<>
-                  <h2 className='text-2xl font-semibold'>Categories</h2>
-                  <ul className='flex flex-wrap max-w-xs mt-2'>
-                    {postData.categories.map((category) => (
-                      <Link key={category.slug} href={`/categories/${category.slug}`}>
-                        <a>
-                          <li className='flex items-center justify-between px-4 py-2 my-2 mr-3 text-sm font-medium text-gray-700 bg-white rounded-md dark:bg-gray-900 dark:text-gray-100'>
-                            <span>{category.name}</span>
-                          </li>
-                        </a>
-                      </Link>
-                    ))}
-                  </ul>
-                </>
-                )
-                : ""
-              }
+        {
+          postData.posts.length > 0 && postData.categories.length > 0 ? (
+            <div className='flex justify-center w-full h-full text-gray-900 md:px-4 dark:text-gray-100'>
+              <div className='w-full md:w-3/5 lg:w-2/5'>
+                {
+                  postData.posts.map((post) => (
+                    <Card key={post.slug} post={post} />
+                  ))
+                }
+              </div>
+              <aside className='hidden md:block'>
+                <div className='ml-6'>
+                  {postData.categories.length > 0 ?
+                    (<>
+                      <h2 className='text-2xl font-semibold'>Categories</h2>
+                      <ul className='flex flex-wrap max-w-xs mt-2'>
+                        {postData.categories.map((category) => (
+                          <Link key={category.slug} href={`/categories/${category.slug}`}>
+                            <a>
+                              <li className='flex items-center justify-between px-4 py-2 my-2 mr-3 text-sm font-medium text-gray-700 bg-white rounded-md dark:bg-gray-900 dark:text-gray-100'>
+                                <span>{category.name}</span>
+                              </li>
+                            </a>
+                          </Link>
+                        ))}
+                      </ul>
+                    </>
+                    )
+                    : ""
+                  }
+                </div>
+              </aside>
             </div>
-          </aside>
-        </div>
-      </MainLayout>
-    </div>
+          ) :
+            (<div>There is nothing here...</div>)
+        }
+      </MainLayout >
+    </div >
   );
 };
