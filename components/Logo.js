b8bd103d9fda8f3c7a529d0ded499e97cb77fb64
Manish Shivabhakti <manish@learnr.app>
2022-11-22 23:28:49 +0545

    Temp: Temporary Logo change

--- components/Logo.js
+++ components/Logo.js
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <Link href="/">
       <a className="flex text-2xl font-semibold lg:text-3xl">
-        <h1>Learnr</h1><span className="text-green-600">.</span>
+        <h1>CMS</h1><span className="text-indigo-600">.</span>
       </a>
     </Link>
   );
