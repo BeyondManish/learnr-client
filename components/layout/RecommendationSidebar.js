40ce6e5c14c34ebb82bbbd429794ed54c167fab8
Manish Shivabhakti <manish@learnr.app>
2023-01-26 11:31:29 +0545

    Update the system of sidebar

--- components/layout/RecommendationSidebar.js
+++ components/layout/RecommendationSidebar.js
@@ -1,10 +1,10 @@
 export default function RecommendationSideBar() {
   return (
-    <div className="flex flex-col w-full h-full bg-white border border-gray-300 rounded-lg">
-      <div className='flex items-center p-4 text-xl font-semibold border-b border-gray-300'>
+    <div className="flex flex-col w-full h-full bg-white border rounded-lg dark:bg-gray-900">
+      <div className='flex items-center p-4 text-xl font-semibold border-b border-gray-200'>
         Recommendation
       </div>
-      <div className='p-4 border-b border-gray-300 cursor-pointer'>
+      <div className='p-4 border-b border-gray-100 cursor-pointer'>
         Hello world!
       </div>
     </div>
